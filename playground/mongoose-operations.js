1f605416c34bd127c1f13851dcdbd953d5537f16
dwijesh daka <dwijesh.daka@gmail.com>
2018-11-19 22:16:11 +0530

    add post /todos and refactor mongoose

---
+++ playground/mongoose-operations.js
@@ -0,0 +1,23 @@
+/*
+var newTodo = new Todo({
+    text : '  a   '
+});
+
+newTodo.save().then((res) => {
+    console.log('Saved todo succesfully',JSON.stringify(res,undefined,2));
+},(err) => {
+    console.log('unable to add todo',err);
+});
+
+
+var otherTodo = new Todo({
+    text : 'lunch',
+    completed : 'true',
+    completedAt : '1856'
+});
+
+otherTodo.save().then((res) => {
+    console.log('Saved todo succesfully',res);
+},(err)=>{
+    console.log('unable to add todo',err);
+});   */
